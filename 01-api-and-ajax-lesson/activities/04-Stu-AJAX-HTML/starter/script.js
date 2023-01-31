d8ec79e7886c6d1cd57300be3ee229577c20382f
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-01-31 20:52:38 +0000

    exercise 4 completed and cleaned up for nice code

--- 01-api-and-ajax-lesson/activities/04-Stu-AJAX-HTML/starter/script.js
+++ 01-api-and-ajax-lesson/activities/04-Stu-AJAX-HTML/starter/script.js
@@ -1,18 +1,33 @@
 // The below code fills in the first row of the table
-var movie = "Dune";
-var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+function createrow(titlename,movieyear,movieactors){
+  // Create and save a reference to new empty table row
+  const newrow = $('<tr>')
+  // Create and save references to 3 td elements containing the Title, Year, and Actors from the AJAX response object
+  const name = $('<td>').text(titlename)
+  const year = $('<td>').text(movieyear)
+  const actors = $('<td>').text(movieactors)
+  // Append the td elements to the new table row
+  newrow.append(name,year,actors)
+  // Append the table row to the tbody element
+  $("tbody").append(newrow)
+}
+
+function addmovie(moviename){
+  var queryURL = "https://www.omdbapi.com/?t=" + moviename + "&apikey=trilogy";
 
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
-
-  // Create and save a reference to new empty table row
-  // Create and save references to 3 td elements containing the Title, Year, and Actors from the AJAX response object
-  // Append the td elements to the new table row
-  // Append the table row to the tbody element
+createrow(response.Title,response.Year,response.Actors)
 });
+}
+
+addmovie("Dune")
 
 // Repeat the above logic to add rows for two more movies of your choice
+addmovie("About+a+boy")
+
+addmovie("Gladiator")
 
 // BONUS: Once complete, try to make your code as DRY as possible through the use of functions
